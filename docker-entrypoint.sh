1d928ce6f36a841e63f241ad02bd1c1239fcd462
Petr Sabanov <pasabanov@murena.io>
2026-07-29 21:04:40 +0300

    Changed hard-coded upstream DNS to ${LOKINET_UPSTREAM_DNS}"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "nameserver 127.3.2.1" > /tmp/resolv.conf
-echo "nameserver 9.9.9.9" >> /tmp/resolv.conf
-cat /tmp/resolv.conf > /etc/resolv.conf || true
+{
+	echo "nameserver 127.3.2.1"
+	[ -n "${LOKINET_UPSTREAM_DNS}" ] &&
+		echo "nameserver ${LOKINET_UPSTREAM_DNS}"
+} > /etc/resolv.conf
 echo "DNS forcibly set to 127.3.2.1"
 
 mkdir -p /var/lib/lokinet
